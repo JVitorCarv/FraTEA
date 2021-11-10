edf4c6a40456d665a21a7c90ef3377a1211f1005
Vitor Carvalho <vitor.velozo2003@gmail.com>
2021-11-10 12:30:12 -0300

    implements ids for answers and changes words storages to dictionaries

--- game02.py
+++ game02.py
@@ -1,9 +1,10 @@
 import random
 
-subjects = ['CARLOS', 'MATHEUS', 'ANA']
-verbs = ['COMEU', 'GOSTA DE', 'COMPROU']
-object = ['MAÇÃ', 'PIZZA', 'FEIJÃO']
+subjects = {'CARLOS':'subject01', 'MATHEUS':'subject02', 'ANA':'subject03'}
+verbs = {'COMEU':'verbs01', 'GOSTA DE':'verbs02', 'COMPROU':'verbs03'}
+objects = {'MAÇÃ':'objects01', 'PIZZA':'objects02', 'FEIJÃO':'objects03'}
 
-answer = (f'{random.choice(subjects)} {random.choice(verbs)} {random.choice(object)}')
+#Pega as keys dos respectivos dicionários e os transforma numa lista para serem escolhidos pelo random.choice
+answer_correct = (f'{random.choice(list(subjects.keys()))} {random.choice(list(verbs.keys()))} {random.choice(list(objects.keys()))}')
 
-print(answer)
+print(answer_correct)
