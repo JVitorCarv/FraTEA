5ff722b5811b6f886b48a3110aa256a1f88d76ce
Vitor Carvalho <vitor.velozo2003@gmail.com>
2021-11-23 10:47:16 -0300

    changes phrases to words

--- game01.py
+++ game01.py
@@ -1,9 +1,7 @@
-#Utilizamos a biblioteca random para escolhermos palavras das listas
+#Utilizamos a biblioteca random para escolhermos "imagens" das listas
 import random
 
-list_subjects = ['Rodrigo', 'Eduarda', 'Luiz']
-list_verbs = ['come', 'gosta de']
-list_objects = ['maçã', 'uva', 'pera']
+list_pictures = ['Imagem de maçã', 'Imagem de uva', 'Imagem de pera']
 
 print('Bem vindo ao exercício! Para sair, informe "SAIR"')
 
@@ -11,22 +9,15 @@ answer_user = ''
 
 #Enquanto a frase informada não for SAIR, o código monta a resposta
 while answer_user != 'SAIR':
-    subject = random.choice(list_subjects)
-    print(subject)
-
-    verb = random.choice(list_verbs)
-    print(verb)
-
-    object = random.choice(list_objects)
-    print(object)
-    
-    answer_correct = str(subject + ' ' + verb + ' ' + object)
-    answer_correct = answer_correct.upper() #dessa forma, a resposta não é case sensitive
+    picture = random.choice(list_pictures)
+    answer_correct = picture.upper()
+    answer_correct = answer_correct[10::]
 
     #Pede a entrada da frase enquanto for diferente da resposta e de SAIR
     #Se uma dessas condições não for satisfeita, o loop é quebrado
     while answer_user != answer_correct and answer_user != 'SAIR':
-        answer_user = str(input('Informe a frase: '))
+        print(picture)
+        answer_user = str(input('O que é isso? '))
         answer_user = answer_user.upper() #a frase também muda para evitar case sensitivity
 
         #Dá um feedback de acordo com a resposta
