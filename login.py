f7ecea66ffba2826a92e29dd3e147704133fa906
Vitor Carvalho <vitor.velozo2003@gmail.com>
2021-11-24 11:02:55 -0300

    removes avatar from login.py

--- login.py
+++ login.py
@@ -1,12 +1,18 @@
 print(13*'=' + '\nVAMOS COMEÇAR\n' + 13*'=')
-print('\nOi, eu sou o FraTEA!\n\nEu vou ser o seu guia nessa aventura incrível!\n')
 
-username = input('Qual o seu nome? ')
-
-list_avatar = [1, 2, 3]
-chosen_avatar = int(input((f'{username}, antes de começar, escolha seu avatar:\n(digite um número de 1 a 3) ')))
-
-print('''Pronto!\nAgora vamos aprender a jogar: \n
-Neste jogo, nós vamos aprender sobre alguns dos elementos básicos que formam as sentenças do português.\n
-Você só precisa arrastar as imagens para os seus lugares correspondentes nas sentenças.\n
-Vamos aprender brincando!\n''')
+option = ''
+while option != '1' and option != '2':
+    option = input((f'Deseja começar?\n1 - Para sim\n2 - Para não\nInsira aqui: '))
+    #Ensina/relembra como jogar antes de continuar
+    if option == '1':
+        print('''Pronto!\nAgora vamos aprender a jogar: \n
+        Neste jogo, nós vamos aprender sobre alguns dos elementos básicos que formam as sentenças do português.\n
+        Você só precisa arrastar as imagens para os seus lugares correspondentes nas sentenças.\n
+        Vamos aprender brincando!\n''')
+    #Encerra a aplicação
+    elif option == '2':
+        print('Até mais!')
+        exit()
+    #No caso do usuário inserir alguma opção inválida
+    else:
+        print('Não entendi o que você informou... \n\nPor favor tente novamente')
